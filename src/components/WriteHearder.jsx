7bdd025d599ab6253c2d0ebcd02bacf0593c5de9
jigong2024 <choijming21@naver.com>
2024-08-29 12:26:38 +0900

    add: 헤더 생성

---
+++ src/components/WriteHearder.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WriteHearder = () => {
+  return <div>WriteHearder</div>;
+};
+
+export default WriteHearder;
--- src/pages/Writing.jsx
+++ src/pages/Writing.jsx
@@ -2,12 +2,16 @@ import styled from 'styled-components';
 import WriteFormContainer from '../components/WriteFormContainer';
 import WriteSidebar from '../components/WriteSidebar';
 import '../assets/Writing.css';
+import WriteHearder from '../components/WriteHearder';
 
 const Writing = () => {
   return (
     <WritingPage>
       <WriteSidebar />
-      <WriteFormContainer />
+      <div>
+        <WriteHearder />
+        <WriteFormContainer />
+      </div>
     </WritingPage>
   );
 };
