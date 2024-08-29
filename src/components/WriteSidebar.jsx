ec48f53936e9fb6c24ac46bce6be90465bc5bfac
jigong2024 <choijming21@naver.com>
2024-08-29 12:23:10 +0900

    update: 사이드바 기본UI 구성

--- src/components/WriteSidebar.jsx
+++ src/components/WriteSidebar.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const WriteSidebar = () => {
-  return <div>WriteSidebar</div>;
+  return (
+    <Sidebar>
+      <h1>맛있는 발견</h1>
+      <LogoutContainer>
+        <h3>Logout</h3>
+        <h3>/</h3>
+        <h3>My page</h3>
+      </LogoutContainer>
+
+      <div>
+        <input />
+        <button>click</button>
+      </div>
+      <TextContainer>
+        <h2>Home</h2>
+        <h2>내 게시글</h2>
+      </TextContainer>
+    </Sidebar>
+  );
 };
 
 export default WriteSidebar;
+
+const Sidebar = styled.div`
+  width: 35%;
+  height: 100vh;
+  border: 2px solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 30px;
+  padding: 10px;
+`;
+
+const LogoutContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-top: 30px;
+`;
