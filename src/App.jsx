e1ef9fe73a363775cb6d2cfd37c70b48b30f691e
jigong2024 <choijming21@naver.com>
2024-08-29 19:38:14 +0900

    add: 글로벌스타일 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Router from './router/Router';
 import './reset.css';
+import GlobalStyles from './GlobalStyles.jxs';
 
 const App = () => {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyles />
+      <Router />
+    </>
+  );
 };
 
 export default App;
