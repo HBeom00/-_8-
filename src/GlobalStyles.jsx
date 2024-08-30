cfab6af781c0ff05d287ec61fa699daf2c1d3672
boyoung-12 <boyoung182@gmail.com>
2024-08-30 19:19:49 +0900

    update:login기능구현완료,signup계속진행

---
+++ src/GlobalStyles.jsx
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+html{
+    height: 100%;
+    
+
+}
+
+
+#root,
+
+body{
+    height: 100%;
+ }
+`;
+
+export default GlobalStyles;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,163 @@
+import styled from 'styled-components';
+import supabase from '../supabaseClient';
+import { useState } from 'react';
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [users, setUsers] = useState([]);
+  const [info, setInfo] = useState({
+    email: '',
+    password: '',
+    nickname: '',
+    introduction: ''
+  });
+
+  const handleChangeInput = (event, keyName) => {
+    const value = event.target.value;
+    const newInfo = { ...info, [keyName]: value };
+    setInfo(newInfo);
+  };
+
+  const handleAddInfo = (event) => {
+    event.preventDefault();
+    signUpUser();
+  };
+
+  async function signUpUser() {
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: info.email,
+        password: info.password,
+        options: {
+          data: {
+            nickname: info.nickname,
+            introduction: info.introduction
+          }
+        }
+      });
+
+      if (error) {
+        throw error;
+      }
+
+      console.log('User signed up:', data);
+    } catch (error) {
+      console.error('Error signing up:', error);
+    }
+  }
+
+  return (
+    <div>
+      <GotoBackbutton>back</GotoBackbutton>
+      <div
+        type="img"
+        value={info.img}
+        onChange={(event) => {
+          handleChangeInput(event, 'img');
+        }}
+      >
+        사진들어감
+      </div>
+      <LoginContainer>
+        <div>
+          <LoginTitle>Sign up</LoginTitle>
+        </div>
+        <InputBoxs>
+          <LoginInputForId
+            type="text"
+            value={info.email}
+            onChange={(event) => {
+              handleChangeInput(event, 'email');
+            }}
+            placeholder="Email"
+          />
+          <LoginInputForPassWord
+            type="text"
+            value={info.password}
+            onChange={(event) => {
+              handleChangeInput(event, 'password');
+            }}
+            placeholder="Password"
+          />
+          <LoginInputForNickName
+            type="text"
+            value={info.nickname}
+            onChange={(event) => {
+              handleChangeInput(event, 'nickname');
+            }}
+            placeholder="Nickname"
+          />
+          <LoginInputForIntroduction
+            type="text"
+            value={info.introduction}
+            onChange={(event) => {
+              handleChangeInput(event, 'introduction');
+            }}
+            placeholder="Introduce yourself"
+          />
+        </InputBoxs>
+        <ButtonBoxs>
+          <LoginSignUpButton type="button" onClick={handleAddInfo}>
+            Sign up
+          </LoginSignUpButton>
+        </ButtonBoxs>
+      </LoginContainer>
+    </div>
+  );
 };
 
 export default SignUp;
+
+const GotoBackbutton = styled.button`
+  width: 60px;
+  height: 30px;
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  background-color: green;
+  gap: 40px;
+  width: 308px;
+`;
+
+const LoginTitle = styled.h1`
+  font-size: 50px;
+`;
+
+const InputBoxs = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const ButtonBoxs = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-content: flex-end;
+  width: 308px;
+`;
+
+const LoginInputForId = styled.input`
+  width: 300px;
+  height: 30px;
+`;
+const LoginInputForPassWord = styled.input`
+  width: 300px;
+  height: 30px;
+`;
+
+const LoginInputForNickName = styled.input`
+  width: 300px;
+  height: 30px;
+`;
+
+const LoginInputForIntroduction = styled.input`
+  width: 300px;
+  height: 30px;
+`;
+
+const LoginSignUpButton = styled.button`
+  width: 100px;
+  height: 30px;
+`;
