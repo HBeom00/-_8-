a30ce83530a820560916c525b3fdf40f96b79aaa
jigong2024 <choijming21@naver.com>
2024-09-03 18:19:09 +0900

    폰트 추가

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -1,14 +1,37 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-html{
+  @font-face {
+    font-family: 'Okticon';
+    src: url('/fonts/okticon_bold.ttf') format('truetype');
+    font-weight: bold;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Okticon';
+    src: url('/fonts/okticon_light.ttf') format('truetype');
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Okticon';
+    src: url('/fonts/okticon_regular.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  html {
     height: 100%;
-}
+    font-family: 'Okticon', sans-serif;
+  }
 
-#root,
-body{
+  #root, body {
     width: 100%;
+    font-family: 'Okticon', sans-serif;
 
-}
+  }
 `;
+
 export default GlobalStyles;
