d4fa95051ef49c2b5ca77e434af3d7fb398b45b1
Hbeom00 <bum6968@naver.com>
2024-09-04 10:16:25 +0900

    찐최종

--- src/context/MypageContext.jsx
+++ src/context/MypageContext.jsx
@@ -5,12 +5,7 @@ export const PostContext = createContext();
 const PostProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
   const [profileUrl, setProfileUrl] = useState('');
-  const [isSelect, setIsSelect] = useState('myinfo');
 
-  return (
-    <PostContext.Provider value={{ posts, setPosts, profileUrl, setProfileUrl, isSelect, setIsSelect }}>
-      {children}
-    </PostContext.Provider>
-  );
+  return <PostContext.Provider value={{ posts, setPosts, profileUrl, setProfileUrl }}>{children}</PostContext.Provider>;
 };
 export default PostProvider;
